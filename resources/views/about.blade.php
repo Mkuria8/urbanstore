a284e24164c6cbb02fded88de3522ca5c327756c
M.Kuria <manasseskush@gmail.com>
2025-03-23 21:13:23 +0300

    I developed the mvp for the urbanstore

---
+++ resources/views/about.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+@section('content')
+    <main class="pt-90">
+        <div class="mb-4 pb-4"></div>
+        <section class="contact-us container">
+            <div class="mw-930">
+                <h2 class="page-title">About US</h2>
+            </div>
+
+            <div class="about-us__content pb-5 mb-5">
+                <p class="mb-5">
+                    <img loading="lazy" class="w-100 h-auto d-block" src="assets/images/about/about-1.jpg" width="1410"
+                        height="550" alt="" />
+                </p>
+                <div class="mw-930">
+                    <h3 class="mb-4">OUR STORY</h3>
+
+                    <p class="mb-4">Urbanstore is your go-to online destination for the latest trends in fashion, 
+                        lifestyle, and technology. We are passionate about providing high-quality,
+                         stylish products that fit seamlessly into the urban lifestyle. From trendy
+                          apparel and accessories to cutting-edge gadgets, Urbanstore brings you the best in
+                           contemporary living.</p>
+                    <div class="row mb-3">
+                        <div class="col-md-6">
+                            <h5 class="mb-3">Our Mission</h5>
+                            <p class="mb-3">At Urbanstore, our mission is simple: to offer urban dwellers a curated selection of products that elevate their style, enhance their lives, and embrace the spirit of modern living. 
+                                We strive to create an online shopping experience that is effortless, enjoyable, and fast—while ensuring that every product we offer reflects our commitment to quality and innovation.</p>
+                        </div>
+                        <div class="col-md-6">
+                            <h5 class="mb-3">Our Vision</h5>
+                            <p class="mb-3">We envision Urbanstore as more than just a store—we aim to be a hub for inspiration, community, and style. 
+                                Through our curated collections, we want to empower individuals to express their unique urban personalities while embracing the vibrant, ever-evolving culture of city life.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="mw-930 d-lg-flex align-items-lg-center">
+                    <div class="image-wrapper col-lg-6">
+                        <img class="h-auto" loading="lazy" src="assets/images/about/about-1.jpg" width="450"
+                            height="500" alt="">
+                    </div>
+                    <div class="content-wrapper col-lg-6 px-lg-4">
+                        <h5 class="mb-3">The Company</h5>
+                        <p>Why Choose Us?
+Quality & Style: We select only the finest products that align with the latest trends and urban aesthetics.
+
+Fast & Reliable Shipping: We offer quick delivery options to ensure that you get your products when you need them.
+
+Customer-Centric Approach: Our goal is to make your shopping experience seamless and enjoyable, from browsing to checkout and beyond.
+
+Affordable Prices: Quality doesn’t have to come with a hefty price tag. We believe in offering competitive prices without compromising on the experience.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+@endsection
