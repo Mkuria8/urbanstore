3dcf93b8e000e142d71adcaaad0e0ddca992085a
M.Kuria <manasseskush@gmail.com>
2025-03-24 05:05:16 +0300

    updated index.php  file

--- public/index.php
+++ public/index.php
@@ -12,6 +12,15 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+// Bootstrap the Laravel application
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// Handle the incoming request using the HTTP kernel
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(Request::capture());
+
+// Send the response to the browser
+$response->send();
+
+// Terminate the request
+$kernel->terminate(Request::capture(), $response);
